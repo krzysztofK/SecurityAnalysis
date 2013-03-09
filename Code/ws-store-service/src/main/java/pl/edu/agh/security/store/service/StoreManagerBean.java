1aa65e21af25efdd1e024125690b47bcef0de369
Krzysztof Wilaszek <k.wilaszek@gmail.com>
2013-03-09 17:18:40 +0100

    Not working ws authentication

--- Code/ws-store-service/src/main/java/pl/edu/agh/security/store/service/StoreManagerBean.java
+++ Code/ws-store-service/src/main/java/pl/edu/agh/security/store/service/StoreManagerBean.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.security.store.service;
 
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
@@ -7,20 +9,25 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.ParameterStyle;
 
+import org.jboss.ejb3.annotation.SecurityDomain;
+
 import pl.edu.agh.security.store.interfaces.IStoreManager;
 
 /**
  * @author Krzysztof
  * 
  */
+//@SecurityDomain("ws-ldap-saml")
+//@DeclareRoles({ "magister" })
+//@RolesAllowed({ "magister" })
 @Stateless
 @Remote(IStoreManager.class)
-@WebService(name = "StoreManager", serviceName = "StoreManager")
+@WebService(name = "StoreManager", serviceName = "StoreManagerService")
 @SOAPBinding(parameterStyle = ParameterStyle.BARE)
 public class StoreManagerBean implements IStoreManager {
 
 	@WebMethod
 	public StoreStateResponse getStoreState(StoreStateRequest storeStateRequest) {
-		return new StoreStateResponse(storeStateRequest.getProduct(), 20);
+		return new StoreStateResponse(storeStateRequest.getProduct(), 22);
 	}
 }
