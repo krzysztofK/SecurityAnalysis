e88e515bfaf0ef078eba3a85c07b7cfd7f310088
Tomasz Wojcik <tomekwo@tlen.pl>
2013-03-20 14:18:29 +0100

    Warehouse service fix

--- Code/warehouse-service/src/main/java/pl/edu/agh/security/deps/warehouse/WarehouseBean.java
+++ Code/warehouse-service/src/main/java/pl/edu/agh/security/deps/warehouse/WarehouseBean.java
@@ -1,8 +1,20 @@
 package pl.edu.agh.security.deps.warehouse;
 
 import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.jws.HandlerChain;
 import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
 
+import org.jboss.ejb3.annotation.SecurityDomain;
+
+@Stateless
+@WebService(name = "Warehouse", serviceName = "WarehouseService")
+@SOAPBinding(parameterStyle = ParameterStyle.BARE)
+@SecurityDomain("ws-ldap-saml")
+@HandlerChain(file = "/META-INF/handlers.xml")
 public class WarehouseBean {
 
     @RolesAllowed({ "magister" })
