400bc241b64ea372daf8a0ae898b23e62b1e7fa4
Tomasz Wojcik <tomekwo@tlen.pl>
2013-06-04 10:26:57 +0200

    Test client added

--- Code/test-client/src/main/java/pl/edu/agh/security/test/client/App.java
+++ Code/test-client/src/main/java/pl/edu/agh/security/test/client/App.java
@@ -1,16 +1,25 @@
 package pl.edu.agh.security.test.client;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.ext.ClientFactory;
+import java.io.StringWriter;
+
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+
+import pl.edu.agh.security.delivery.marshallers.TestDataMarshaller;
 
 /**
  * Hello world!
  * 
  */
 public class App {
-	public static void main(String[] args) {
-		Client client = ClientFactory.newClient();
-		String resp = client.target("localhost:8080/delivery-service/delivery").request().get(String.class);
-		System.out.println("resp: "+resp);
+	public static void main(String[] args) throws Exception {
+	    ClientRequest request = new ClientRequest("http://localhost:8080/delivery-service/rest/deliveries/create");
+	    request.body("application/xml", TestDataMarshaller.createSamplePayload());
+	    ClientResponse<String> response = request.post(String.class);
+	    if (response.getStatus() == 200) // OK!
+	    {
+	       String str = response.getEntity();
+	       System.out.println("Received response: \n"+str);
+	    }
 	}
 }
