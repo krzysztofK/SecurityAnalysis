11d22c67bb200e9a5267c56f048f4b90e9f023aa
Tomasz Wojcik <tomekwo@tlen.pl>
2013-10-14 14:57:51 +0200

    Fully functional Web SSO added

--- Code/stores-state-service/src/main/java/pl/edu/agh/security/rest/interceptor/SecurityInterceptor.java
+++ Code/stores-state-service/src/main/java/pl/edu/agh/security/rest/interceptor/SecurityInterceptor.java
@@ -5,6 +5,7 @@ import javax.security.auth.login.LoginException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.ext.Provider;
 
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.interception.SecurityPrecedence;
 import org.jboss.resteasy.annotations.interception.ServerInterceptor;
 import org.jboss.resteasy.core.ResourceMethod;
@@ -17,11 +18,14 @@ import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
 @ServerInterceptor
 @SecurityPrecedence
 public class SecurityInterceptor implements PreProcessInterceptor {
+    
+    private Logger LOGGER = Logger.getLogger(SecurityInterceptor.class);
 		
 	@Override
 	public ServerResponse preProcess(HttpRequest request, ResourceMethod method)
 			throws Failure, WebApplicationException {
 		try {
+		    LOGGER.info("Trying to authenticate "+request.getHttpMethod()+ " request");
 			LoginContext loginContext = new LoginContext("ws-ldap-saml");
 			loginContext.login();
 		} catch (LoginException e) {
--- Code/stores-state-service/src/main/java/pl/edu/agh/security/store/state/objects/id/ProductCountInStoreId.java
+++ Code/stores-state-service/src/main/java/pl/edu/agh/security/store/state/objects/id/ProductCountInStoreId.java
@@ -44,4 +44,36 @@ public class ProductCountInStoreId implements Serializable {
 		return serialVersionUID;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime * result + ((store == null) ? 0 : store.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductCountInStoreId other = (ProductCountInStoreId) obj;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (store == null) {
+			if (other.store != null)
+				return false;
+		} else if (!store.equals(other.store))
+			return false;
+		return true;
+	}
+
+	
 }
